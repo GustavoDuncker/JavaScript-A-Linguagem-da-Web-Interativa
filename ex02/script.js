68b91a48bf59ca499462b095c1bb62e58dd9b9a2
GustavoDuncker <Gustavo_duncker@estudante.sesisenai.org.br>
2025-03-18 09:22:55 -0300

    ex2

---
+++ ex02/script.js
@@ -0,0 +1,12 @@
+var numero1 = prompt("Digite um numero");
+var numero2 = prompt("Digite outro numero");
+var total1=Number (numero1 )+ Number(numero2 )
+var total2=numero1 - numero2 
+var total3=numero1 * numero2 
+var total4=numero1 / numero2 
+
+
+alert (total1)
+alert (total2)
+alert (total3)
+alert (total4)
