083f9e235108473b15cba88eb14cb0bbb9fe343e
GustavoDuncker <Gustavo_duncker@estudante.sesisenai.org.br>
2025-04-04 11:04:55 -0300

    12

---
+++ ex11/script.js
@@ -0,0 +1,9 @@
+function verificarSinal(numero){
+    if (numero = 0) {
+        console.log("Seu numero é 0");
+    
+    }else if(numero>0){
+        console.log("Seu numero é positivo")
+    }
+
+}
